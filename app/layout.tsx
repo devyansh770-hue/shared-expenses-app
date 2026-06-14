b7db4fe636fe8db531498f8e14a96724a6c3aac1
Devyansh Verma <devyansh770@gmail.com>
2026-06-15 03:42:54 +0530

    fix: add suppressHydrationWarning to body to prevent browser extension hydration crashes

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable} ${jetbrainsMono.variable}`}>
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
