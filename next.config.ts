d0fbbdc8998831c17ac75529436411326ff060e5
Devyansh Verma <devyansh770@gmail.com>
2026-06-15 04:14:45 +0530

    chore: disable devIndicators in next.config.ts to prevent UI overlap

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['172.22.124.119'],
+  devIndicators: false,
 };
 
 export default nextConfig;
